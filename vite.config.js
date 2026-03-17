b91bbf9b9cfd21862834f3d653c704620eee4425
KARTHIKRAJ <karthicksubramani97@gmail.com>
2026-03-16 23:40:13 -0400

    initial portfolio

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/karthikraj/',  // ← Change 'portfolio' to your GitHub repo name
+  base: '/karthikrajs/',  // ← Change 'portfolio' to your GitHub repo name
 })
